5ed6182db903c9636e3140c13605b0922c06a0a0
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-09-09 21:50:33 +0900

    add head

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">Hello, world!</h1>
+        <h1>Hello, world!</h1>
       </div>
     );
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "hello, world!!"
+//     )
+//   }
+// }
 
 export default App;
