f5f618768fd601c0d848041132ebb94aa4f1ead6
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-09-09 22:51:41 +0900

    compoennt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => { console.log("I am clicked.") }} />
-      </React.Fragment>
-    );
-  }
+// // app class inherted component
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log("I am clicked.") }} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Myaa</div>
 }
 
 export default App;
