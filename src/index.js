0790a917c53d1ee4a1b734865863565e32399ce4
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-09-10 09:10:20 +0900

    add store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// シングルトンとして定義する(storeにreducerを渡す)
+// componentへstoreを一発で渡す為にproviderを使って渡す事が出来る
+const store = createStore(reducer)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//providerにstoreという属性にstoreを渡してあげる事によってstoreを参照してrenderする事が出来る?
+ReactDOM.render(
+  <Provider store={store}>
+    <App  />
+  </Provider>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
